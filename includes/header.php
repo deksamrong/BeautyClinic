36c8f4a255605099938cd692acb2abf2ad548372
Jarkkapan Boonmak <pladao002@hotmail.com>
2021-05-19 22:37:48 +0700

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">BEAUTY CLINIC</a>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link">หน้าแรก</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">บริการ</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">เกี่ยวกับเรา</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">ติดต่อ</a></li>
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link">เข้าสู่ระบบ</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
