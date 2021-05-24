3fcce6ab6f858aef4de7bd3e3ad92e27d020c6a5
deksamrong <pladao002@hotmail.com>
2021-05-24 19:37:09 +0700

    แก้ไข footer

--- includes/footer.php
+++ includes/footer.php
@@ -59,7 +59,7 @@ while ($row=mysqli_fetch_array($ret)) {
           <div class="col-md-12 text-center">
 
             <p>
-  2019 &copy; Beauty Parlour Management System  </p>
+  2021 &copy; คลินิกเสริมความงาม  </p>
           </div>
         </div>
       </div>
