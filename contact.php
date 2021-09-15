e162a13a06fe3bbe3ab134618000c411c06d7312
Jakkapan <pladao002@gmail.com>
2021-09-15 23:01:04 +0700

    ทดสอบ

--- contact.php
+++ contact.php
@@ -82,12 +82,7 @@ while ($row=mysqli_fetch_array($ret)) {
         </div>
       </div>
     </section>
-			
-	
-
    <?php include_once('includes/footer.php');?>
-    
-
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
